7bc0b1a1f85ecf91a32e90016f459b9f690f43ef
Samuel Virsik <samvirsik@berkeley.edu>
2026-06-09 22:56:41 -0500

    frontend work

--- app.py
+++ app.py
@@ -455,16 +455,6 @@ def index():
     return render_template('landing.html')
 
 
-@app.route('/data')
-def data_page():
-    return render_template('data.html')
-
-
-@app.route('/pricing')
-def pricing_page():
-    return render_template('pricing.html')
-
-
 @app.route('/about')
 def about_page():
     return render_template('about.html')
